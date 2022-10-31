f3338bb47bad1383e27b10f17a8315313b086115
Towsif_aslam <towsifaslam>
2022-10-31 13:53:07 +0600

    Write something about the changes

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,20 @@
+const Contact = () => {
+  return (
+    <div>
+      <p>I am fom Home</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,
+        soluta quos. Ex veniam doloremque ea earum corrupti, id modi qui
+        delectus illo, harum dicta, saepe quidem cumque possimus nesciunt
+        quibusdam vero quod reiciendis vitae tempore. Tempora iste reiciendis
+        quos ab assumenda consequatur ea veritatis sint culpa officia, similique
+        praesentium et?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur
+        nulla, quo nemo culpa mollitia inventore maxime reprehenderit recusandae
+        blanditiis natus!
+      </p>
+    </div>
+  );
+};
